62c4aa09f2110d28e0913a1d4e4062e216bc564f
Jim Collinson <JimCollinson@users.noreply.github.com>
2024-11-01 17:23:18 +0000

    Update about.js
    More Autonomi changes

--- src/containers/about.js
+++ src/containers/about.js
@@ -1,10 +1,10 @@
 // Copyright 2020 MaidSafe.net limited.
 //
-// This SAFE Network Software is licensed to you under the MIT license <LICENSE-MIT
+// This Autonomi Software is licensed to you under the MIT license <LICENSE-MIT
 // http://opensource.org/licenses/MIT> or the Modified BSD license <LICENSE-BSD
 // https://opensource.org/licenses/BSD-3-Clause>, at your option. This file may not be copied,
 // modified, or distributed except according to those terms. Please review the Licences for the
-// specific language governing permissions and limitations relating to use of the SAFE Network
+// specific language governing permissions and limitations relating to use of the Autonomi
 // Software.
 
 import React from 'react';
@@ -81,13 +81,12 @@ class About extends React.Component {
 							<p>
 								Started in 2006 by Scottish engineer David Irvine, MaidSafe is a small team,
   comprised of: thinkers, inventors, tinkerers, PhDs, engineers and designers.
-  Although based in Ayr, Scotland, we have drawn talents from many different
-  cultures and countries, reflecting the users that we serve. Despite this
-  variety, we all share a mission: a desire to provide security and privacy for
+  Although based in Scotland, we have drawn talents from many different
+  cultures and countries, reflecting the users that we serve. We all share a mission: a desire to provide security and privacy for
   everyone.
   </p>
 							<div className="ques">
-								<span>Interested in joining our diverse and growing team, either in Ayr or remotely?</span>
+								<span>Interested in joining our diverse and growing team?</span>
 								<Link to="/careers" className="al-btn invert">VIEW OPEN POSITIONS</Link>
 							</div>
 						</div>
@@ -104,7 +103,7 @@ class About extends React.Component {
 							Online data has become so insecure and data thefts so commonplace that our trust in companies securing our data is almost entirely eroded. More than 2.5 billion personal data records were stolen in 2017.  A new way of managing data is required, and MaidSafe combines several features that provide a level of security not possible using existing methods.</p>
 							<p>Data security not only allows us to keep our credit card details safe, or our medical records secure, but also brings us  privacy. Privacy is misunderstood by many.</p>
 							<p>It’s not simply about protecting secrets or even hiding the things that we shouldn’t be doing. Privacy is simply protecting your ability to choose which personal thoughts, communications and actions you want to share with others. Without privacy, one's ability to communicate, explore, act or even think freely is restrained. We need privacy to enable creativity, and to allow new ideas and forms of expression to flourish.</p>
-							<p>In our quest to provide privacy, security and freedom to everyone, we are creating an Open Source decentralised data network called the SAFE Network - and everyone’s welcome. Join us at <a href="https://safenetwork.tech">safenetwork.tech</a> </p>
+							<p>In our quest to provide privacy, security and freedom to everyone, we are creating an Open Source decentralised data network called Autonomi - and everyone’s welcome. Join us at <a href="https://safenetwork.tech">safenetwork.tech</a> </p>
 						</div>
 					</div>
 					<div className="post-3-cont">
